b26e9ee1839e0a9bab7f7773109671df68f5429e
Kevin Mahoney <kmahoney@theatlantic.com>
2018-01-05 17:30:47 -0500

    initial commit

---
+++ js/clientApp.jsx
@@ -0,0 +1,46 @@
+const myTitle = props => {
+  return React.createElement(
+    "div",
+    { className: "childDiv" },
+    React.createElement(
+      "h1",
+      {
+        className: props.class,
+        style: { color: props.color }
+      },
+      props.title
+    )
+  );
+};
+
+const myFirstComponent = () => {
+  return React.createElement(
+    "div",
+    { className: "first div" },
+    React.createElement(myTitle, {
+      class: "childA",
+      title: "Apple",
+      color: "BlanchedAlmond"
+    }),
+    React.createElement(myTitle, {
+      class: "childB",
+      title: "Banana",
+      color: "BurlyWood"
+    }),
+    React.createElement(myTitle, {
+      class: "childC",
+      title: "Grape",
+      color: "LimeGreen"
+    }),
+    React.createElement(myTitle, {
+      class: "childD",
+      title: "Orange",
+      color: "Lime"
+    })
+  );
+};
+
+ReactDOM.render(
+  React.createElement(myFirstComponent),
+  document.getElementById("app")
+);
