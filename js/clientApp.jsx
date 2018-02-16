898441d7f36c4b7a7a707b9cbcdb773ef756e116
Kevin Mahoney <kmahoney@theatlantic.com>
2018-02-16 13:47:40 -0500

    render using jsx

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,46 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const myTitle = props => {
-  return React.createElement(
-    'div',
-    { className: 'childDiv' },
-    React.createElement(
-      'h1',
-      {
-        className: props.class,
-        style: { color: props.color }
-      },
-      props.title
-    )
-  );
+const MyTitle = props => {
+  const style = {color:props.color}
+  // return React.createElement(
+  //   'div',
+  //   { className: 'childDiv' },
+  //   React.createElement(
+  //     'h1',
+  //     {
+  //       className: props.class,
+  //       style: { color: props.color }
+  //     },
+  //     props.title
+  //   )
+  // );
+  return(
+    <div>
+      <h1 style={ style }>{ props.title.toUpperCase() }</h1>
+    </div>
+  )
 };
 
 const myFirstComponent = () => {
-  return React.createElement(
-    'div',
-    { className: 'first div' },
-    React.createElement(myTitle, {
-      class: 'childA',
-      title: 'Apple',
-      color: 'BlanchedAlmond'
-    }),
-    React.createElement(myTitle, {
-      class: 'childB',
-      title: 'Banana',
-      color: 'BurlyWood'
-    }),
-    React.createElement(myTitle, {
-      class: 'childC',
-      title: 'Grape',
-      color: 'LimeGreen'
-    }),
-    React.createElement(myTitle, {
-      class: 'childD',
-      title: 'Orange',
-      color: 'Lime'
-    })
-  );
+  return(
+   <div id="my-first-component">
+     <MyTitle title="Apple" color="red" />
+     <MyTitle title="Banana" color="yellow" />
+     <MyTitle title="Grape" color="purple" />
+     <MyTitle title="Pear" color="green" />
+   </div>
+  )
 };
 
-render(React.createElement(myFirstComponent), document.getElementById('app'));
+render(<myFirstComponent />, document.getElementById('app'));
